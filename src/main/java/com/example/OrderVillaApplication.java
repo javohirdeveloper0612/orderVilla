596cdba25a8dac8f44b2b144cb9941c61278e89c
javaDev <javoxiryallayev1@gmail.com>
2023-07-11 22:31:12 +0500

    Initial commit

---
+++ src/main/java/com/example/OrderVillaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderVillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderVillaApplication.class, args);
+	}
+
+}
